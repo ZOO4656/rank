77e998d75c7e7b8660a411f10bff674d538b9307
= <=>
2017-03-21 09:38:18 +0900

    Create thumbnails table.

---
+++ database/migrations/2017_03_21_003037_create_thumbnails_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateThumbnailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('thumbnails', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('src');
+            $table->integer('width')->unsigned();
+            $table->integer('height')->unsigned();
+            $table->string('extension');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('thumbnails');
+    }
+}
